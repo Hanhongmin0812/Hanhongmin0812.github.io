bfd84d2da62806af30c8485549423f1b0d553ee6
Hanhongmin0812 <obze1776@gmail.com>
2025-03-20 12:06:15 +0900

    Create homework3-1.php

---
+++ homework3-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$n = 30;
+$sum = 0;
+$prod = 1;
+for ($x = 1; $x < $n+1; $x++) {
+  $sum = $x + $sum;
+  $prod = $prod * $x;
+}
+echo "1부터 30까지의 합은 = $sum<br>";
+echo "1부터 30까지의 곱은 = $prod";
+?>
+
+</body>
+</html>
